0f6434264d24bfdd0cc6bba2e5c5a4bbebb1caaf
wongil12 <judgevi52@gmail.com>
2020-07-31 23:25:16 +0900

    오타 수정

--- Dht/dht.py
+++ Dht/dht.py
@@ -8,7 +8,7 @@ def getHum(dht) :
     hum = dht.humidity
 
 def getDht() :
-    inDht = adafruit_dht.DH11(board.D4)
-    outDht = adafruit_dht.DH11(board.D21)
+    inDht = adafruit_dht.DHT11(board.D4)
+    outDht = adafruit_dht.DHT11(board.D21)
     return getTmp(inDht), getHum(inDht), getTmp(outDht), getHum(outDht)
     
