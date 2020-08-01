01f4b951e2a41288b41498b1ab68dc694023f979
wongil12 <judgevi52@gmail.com>
2020-08-01 09:36:46 +0900

    능-지

--- tempHum/tempHum.py
+++ tempHum/tempHum.py
@@ -4,9 +4,10 @@ import adafruit_dht
 
 def getTmp(dht) :
     tmp = dht.temperature
+    return tmp
 def getHum(dht) :
     hum = dht.humidity
-
+    return hum
 def getDht() :
     inDht = adafruit_dht.DHT11(board.D4)
     outDht = adafruit_dht.DHT11(board.D21)
